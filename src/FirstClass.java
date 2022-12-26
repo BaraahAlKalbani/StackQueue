810e8dcb78d9de35af66bbfff98f00029a707872
BaraahAlKalbani <baraalkalbani@gmail.com>
2022-12-26 00:25:56 -0800

    updated files

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,31 +4,33 @@ public class FirstClass {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-//        Stack<String> aStack = new Stack<>();
-//        aStack.add("first Plate");
-//        aStack.add("second Plate");
-//        aStack.add("third Plate");
-//        aStack.add("forth Plate");
-//        aStack.add("fifth Plate");
-//        aStack.add("sixth Plate");
-//
-//        while (!aStack.empty()) {
-//            System.out.println(aStack.pop());
-//        }
+        // Stack<String> aStack = new Stack<>();
+        // aStack.add("first Plate");
+        // aStack.add("second Plate");
+        // aStack.add("third Plate");
+        // aStack.add("forth Plate");
+        // aStack.add("fifth Plate");
+        // aStack.add("sixth Plate");
+        //
+        // while (!aStack.empty()) {
+        // System.out.println(aStack.pop());
+        // }
 
-        String expString = "}{int=2;}{}{";
+        String expString = "{int x = 2;}{}";
         Stack<Character> stack1 = new Stack<>();
-        boolean valid=scoop(expString, stack1);
-        System.out.println( expString+" is valid:"+valid+"\n");
-        
-        if(valid)
-        {
-            while(!stack1.empty())
-            System.out.print(stack1.pop());
+        boolean valid = scoop(expString, stack1);
+        System.out.println(expString + "\nis valid:" + valid + "\n");
+
+        if (valid) {
+            // while(!stack1.empty())
+            // System.out.print(stack1.pop()); 
+            for (char bracket:stack1) {
+                System.out.print(bracket);
+            }
         }
     }
 
-    public static boolean scoop(String expString,Stack<Character> stack1) {
+    public static boolean scoop(String expString, Stack<Character> stack1) {
         char[] expArray = expString.toCharArray();
         int open = 0;
         int close = 0;
@@ -36,16 +38,15 @@ public class FirstClass {
             if (c == ('{') && open >= close) {
                 open++;
                 stack1.push(c);
-            } 
-            
-            
+            }
+
             if (c == '}') {
                 close++;
                 stack1.push(c);
             }
-            else if (c != '}' && c != '{') {
-                stack1.push(c);
-            }
+            // else if (c != '}' && c != '{') {
+            // stack1.push(c);
+            // }
         }
         if (open == close) {
             return true;
